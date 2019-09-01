d826e8cf1d2e85cf6d13a800700ecfc48b24b128
bankonly <47017349+bankonly@users.noreply.github.com>
2019-09-01 16:44:31 +0700

    pull

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
 from route.api import *
 from route.web import *
 
+
+from config.db import Config
+Config.db.init_app(app)
+
+
 if __name__ == "__main__":
-    from config.db import Config
-    Config.db.init_app(app)
     app.run(port=5000)
