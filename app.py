d7a3e451258798eebb597cbcb9e9637f97189a42
bankonly <47017349+bankonly@users.noreply.github.com>
2019-09-01 16:45:43 +0700

    pull

--- app.py
+++ app.py
@@ -5,6 +5,10 @@ from route.web import *
 from config.db import Config
 Config.db.init_app(app)
 
+# create table
+@app.before_first_request
+def create_table():
+    Config.db.create_all()
 
 if __name__ == "__main__":
     app.run(port=5000)
